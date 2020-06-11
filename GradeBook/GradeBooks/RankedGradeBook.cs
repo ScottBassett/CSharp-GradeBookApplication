513d2b0b2dabf2b36cba1539bfd8bc275921d879
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-06-11 16:34:17 +0100

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,15 +17,27 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
-                throw new InvalidOperationException(message:"Not enough students!");
-            }
+                throw new InvalidOperationException(message: "Not enough students!");
 
             // Figure out how many students it takes to drop a letter grade (20% of the total number of students)
             // Put all the average grades in order, then figure out where the input grade would fit in the series of grades
             // Every X students with higher grades than the input grade knocks the letter grade down until you reach F.
 
-            return 'F';
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade)
+                                           .Select(e => e.AverageGrade)
+                                           .ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
