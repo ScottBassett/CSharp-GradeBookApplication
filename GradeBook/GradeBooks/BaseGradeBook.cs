698ba05146e686da8c34be4c8d5d6c4f05b6ca8c
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-06-12 14:00:39 +0100

    Completed module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,11 +16,11 @@ namespace GradeBook.GradeBooks
         public GradeBookType Type { get; set; }
         public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
-            IsWeighted = true;
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
